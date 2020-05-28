4c7ce1208f1d34bd79f835ef95e7aa92b292e2e7
wjunnneg <1194348056@qq.com>
2020-05-28 17:01:23 +0800

    update remove red line.

--- src/cores/run_seq2seq.py
+++ src/cores/run_seq2seq.py
@@ -18,17 +18,18 @@ import torch
 from torch.utils.data import RandomSampler
 from torch.utils.data.distributed import DistributedSampler
 
-from pytorch_pretrained_bert.tokenization import BertTokenizer, WhitespaceTokenizer
-from pytorch_pretrained_bert.modeling import BertForPreTrainingLossMask
-from pytorch_pretrained_bert.optimization import BertAdam, warmup_linear
+# WordpieceTokenizer -> WordpieceTokenizer
+from pytorch_pretrained_bert.tokenization import BertTokenizer, WordpieceTokenizer
+# BertForPreTrainingLossMask -> BertForPreTraining
+from pytorch_pretrained_bert.modeling import BertForPreTraining
+# warmup_linear -> WarmupLinearSchedule
+from pytorch_pretrained_bert.optimization import BertAdam, WarmupLinearSchedule
 
 from src.libs.data_parallel import DataParallelImbalance
 import src.cores.seq2seq_loader as seq2seq_loader
 import torch.distributed as dist
 
-
-logging.basicConfig(format='%(asctime)s - %(levelname)s - %(name)s -   %(message)s',
-                    datefmt='%m/%d/%Y %H:%M:%S',
+logging.basicConfig(format='%(asctime)s - %(levelname)s - %(name)s -   %(message)s', datefmt='%m/%d/%Y %H:%M:%S',
                     level=logging.INFO)
 logger = logging.getLogger(__name__)
 
@@ -266,14 +267,27 @@ def main():
         args.bert_model, do_lower_case=args.do_lower_case)
     if args.max_position_embeddings:
         tokenizer.max_len = args.max_position_embeddings
-    data_tokenizer = WhitespaceTokenizer() if args.tokenized_input else tokenizer
+    data_tokenizer = WordpieceTokenizer() if args.tokenized_input else tokenizer
     if args.local_rank == 0:
         dist.barrier()
 
     if args.do_train:
         print("Loading Train Dataset", args.data_dir)
         bi_uni_pipeline = [seq2seq_loader.Preprocess4Seq2seq(args.max_pred, args.mask_prob, list(tokenizer.vocab.keys(
-        )), tokenizer.convert_tokens_to_ids, args.max_seq_length, new_segment_ids=args.new_segment_ids, truncate_config={'max_len_a': args.max_len_a, 'max_len_b': args.max_len_b, 'trunc_seg': args.trunc_seg, 'always_truncate_tail': args.always_truncate_tail}, mask_source_words=args.mask_source_words, skipgram_prb=args.skipgram_prb, skipgram_size=args.skipgram_size, mask_whole_word=args.mask_whole_word, mode="s2s", has_oracle=args.has_sentence_oracle, num_qkv=args.num_qkv, s2s_special_token=args.s2s_special_token, s2s_add_segment=args.s2s_add_segment, s2s_share_segment=args.s2s_share_segment, pos_shift=args.pos_shift)]
+        )), tokenizer.convert_tokens_to_ids, args.max_seq_length, new_segment_ids=args.new_segment_ids,
+                                                             truncate_config={'max_len_a': args.max_len_a,
+                                                                              'max_len_b': args.max_len_b,
+                                                                              'trunc_seg': args.trunc_seg,
+                                                                              'always_truncate_tail': args.always_truncate_tail},
+                                                             mask_source_words=args.mask_source_words,
+                                                             skipgram_prb=args.skipgram_prb,
+                                                             skipgram_size=args.skipgram_size,
+                                                             mask_whole_word=args.mask_whole_word, mode="s2s",
+                                                             has_oracle=args.has_sentence_oracle, num_qkv=args.num_qkv,
+                                                             s2s_special_token=args.s2s_special_token,
+                                                             s2s_add_segment=args.s2s_add_segment,
+                                                             s2s_share_segment=args.s2s_share_segment,
+                                                             pos_shift=args.pos_shift)]
         file_oracle = None
         if args.has_sentence_oracle:
             file_oracle = os.path.join(args.data_dir, 'train.oracle')
@@ -282,7 +296,8 @@ def main():
         fn_tgt = os.path.join(
             args.data_dir, args.tgt_file if args.tgt_file else 'train.tgt')
         train_dataset = seq2seq_loader.Seq2SeqDataset(
-            fn_src, fn_tgt, args.train_batch_size, data_tokenizer, args.max_seq_length, file_oracle=file_oracle, bi_uni_pipeline=bi_uni_pipeline)
+            fn_src, fn_tgt, args.train_batch_size, data_tokenizer, args.max_seq_length, file_oracle=file_oracle,
+            bi_uni_pipeline=bi_uni_pipeline)
         if args.local_rank == -1:
             train_sampler = RandomSampler(train_dataset, replacement=False)
             _batch_size = args.train_batch_size
@@ -290,7 +305,9 @@ def main():
             train_sampler = DistributedSampler(train_dataset)
             _batch_size = args.train_batch_size // dist.get_world_size()
         train_dataloader = torch.utils.data.DataLoader(train_dataset, batch_size=_batch_size, sampler=train_sampler,
-                                                       num_workers=args.num_workers, collate_fn=seq2seq_loader.batch_list_to_batch_tensors, pin_memory=False)
+                                                       num_workers=args.num_workers,
+                                                       collate_fn=seq2seq_loader.batch_list_to_batch_tensors,
+                                                       pin_memory=False)
 
     # note: args.train_batch_size has been changed to (/= args.gradient_accumulation_steps)
     # t_total = int(math.ceil(len(train_dataset.ex_list) / args.train_batch_size)
@@ -307,7 +324,7 @@ def main():
     recover_step = _get_max_epoch_model(args.output_dir)
     cls_num_labels = 2
     type_vocab_size = 6 + \
-        (1 if args.s2s_add_segment else 0) if args.new_segment_ids else 2
+                      (1 if args.s2s_add_segment else 0) if args.new_segment_ids else 2
     num_sentlvl_labels = 2 if args.has_sentence_oracle else 0
     relax_projection = 4 if args.relax_projection else 0
     if args.local_rank not in (-1, 0):
@@ -317,8 +334,13 @@ def main():
         # if _state_dict == {}, the parameters are randomly initialized
         # if _state_dict == None, the parameters are initialized with bert-init
         _state_dict = {} if args.from_scratch else None
-        model = BertForPreTrainingLossMask.from_pretrained(
-            args.bert_model, state_dict=_state_dict, num_labels=cls_num_labels, num_rel=0, type_vocab_size=type_vocab_size, config_path=args.config_path, task_idx=3, num_sentlvl_labels=num_sentlvl_labels, max_position_embeddings=args.max_position_embeddings, label_smoothing=args.label_smoothing, fp32_embedding=args.fp32_embedding, relax_projection=relax_projection, new_pos_ids=args.new_pos_ids, ffn_type=args.ffn_type, hidden_dropout_prob=args.hidden_dropout_prob, attention_probs_dropout_prob=args.attention_probs_dropout_prob, num_qkv=args.num_qkv, seg_emb=args.seg_emb)
+        model = BertForPreTraining.from_pretrained(
+            args.bert_model, state_dict=_state_dict, num_labels=cls_num_labels, num_rel=0,
+            type_vocab_size=type_vocab_size, config_path=args.config_path, task_idx=3,
+            num_sentlvl_labels=num_sentlvl_labels, max_position_embeddings=args.max_position_embeddings,
+            label_smoothing=args.label_smoothing, fp32_embedding=args.fp32_embedding, relax_projection=relax_projection,
+            new_pos_ids=args.new_pos_ids, ffn_type=args.ffn_type, hidden_dropout_prob=args.hidden_dropout_prob,
+            attention_probs_dropout_prob=args.attention_probs_dropout_prob, num_qkv=args.num_qkv, seg_emb=args.seg_emb)
         global_step = 0
     else:
         if recover_step:
@@ -334,8 +356,13 @@ def main():
             model_recover = torch.load(
                 args.model_recover_path, map_location='cpu')
             global_step = 0
-        model = BertForPreTrainingLossMask.from_pretrained(
-            args.bert_model, state_dict=model_recover, num_labels=cls_num_labels, num_rel=0, type_vocab_size=type_vocab_size, config_path=args.config_path, task_idx=3, num_sentlvl_labels=num_sentlvl_labels, max_position_embeddings=args.max_position_embeddings, label_smoothing=args.label_smoothing, fp32_embedding=args.fp32_embedding, relax_projection=relax_projection, new_pos_ids=args.new_pos_ids, ffn_type=args.ffn_type, hidden_dropout_prob=args.hidden_dropout_prob, attention_probs_dropout_prob=args.attention_probs_dropout_prob, num_qkv=args.num_qkv, seg_emb=args.seg_emb)
+        model = BertForPreTraining.from_pretrained(
+            args.bert_model, state_dict=model_recover, num_labels=cls_num_labels, num_rel=0,
+            type_vocab_size=type_vocab_size, config_path=args.config_path, task_idx=3,
+            num_sentlvl_labels=num_sentlvl_labels, max_position_embeddings=args.max_position_embeddings,
+            label_smoothing=args.label_smoothing, fp32_embedding=args.fp32_embedding, relax_projection=relax_projection,
+            new_pos_ids=args.new_pos_ids, ffn_type=args.ffn_type, hidden_dropout_prob=args.hidden_dropout_prob,
+            attention_probs_dropout_prob=args.attention_probs_dropout_prob, num_qkv=args.num_qkv, seg_emb=args.seg_emb)
     if args.local_rank == 0:
         dist.barrier()
 
@@ -352,9 +379,9 @@ def main():
         except ImportError:
             raise ImportError("DistributedDataParallel")
         model = DDP(model, device_ids=[
-                    args.local_rank], output_device=args.local_rank, find_unused_parameters=True)
+            args.local_rank], output_device=args.local_rank, find_unused_parameters=True)
     elif n_gpu > 1:
-        #model = torch.nn.DataParallel(model)
+        # model = torch.nn.DataParallel(model)
         model = DataParallelImbalance(model)
 
     # Prepare optimizer
@@ -368,7 +395,7 @@ def main():
     ]
     if args.fp16:
         try:
-            #from apex.optimizers.fp16_optimizer import FP16_Optimizer
+            # from apex.optimizers.fp16_optimizer import FP16_Optimizer
             from pytorch_pretrained_bert.optimization_fp16 import FP16_Optimizer_State
             from apex.optimizers.fused_adam import FusedAdam
         except ImportError:
@@ -411,10 +438,11 @@ def main():
 
         model.train()
         if recover_step:
-            start_epoch = recover_step+1
+            start_epoch = recover_step + 1
         else:
             start_epoch = 1
-        for i_epoch in trange(start_epoch, int(args.num_train_epochs)+1, desc="Epoch", disable=args.local_rank not in (-1, 0)):
+        for i_epoch in trange(start_epoch, int(args.num_train_epochs) + 1, desc="Epoch",
+                              disable=args.local_rank not in (-1, 0)):
             if args.local_rank != -1:
                 train_sampler.set_epoch(i_epoch)
             iter_bar = tqdm(train_dataloader, desc='Iter (loss=X.XXX)',
@@ -427,10 +455,12 @@ def main():
                 else:
                     input_ids, segment_ids, input_mask, mask_qkv, lm_label_ids, masked_pos, masked_weights, is_next, task_idx = batch
                     oracle_pos, oracle_weights, oracle_labels = None, None, None
-                loss_tuple = model(input_ids, segment_ids, input_mask, lm_label_ids, is_next, masked_pos=masked_pos, masked_weights=masked_weights, task_idx=task_idx, masked_pos_2=oracle_pos, masked_weights_2=oracle_weights,
+                loss_tuple = model(input_ids, segment_ids, input_mask, lm_label_ids, is_next, masked_pos=masked_pos,
+                                   masked_weights=masked_weights, task_idx=task_idx, masked_pos_2=oracle_pos,
+                                   masked_weights_2=oracle_weights,
                                    masked_labels_2=oracle_labels, mask_qkv=mask_qkv)
                 masked_lm_loss, next_sentence_loss = loss_tuple
-                if n_gpu > 1:    # mean() to average on multi-gpu.
+                if n_gpu > 1:  # mean() to average on multi-gpu.
                     # loss = loss.mean()
                     masked_lm_loss = masked_lm_loss.mean()
                     next_sentence_loss = next_sentence_loss.mean()
@@ -450,9 +480,8 @@ def main():
                 else:
                     loss.backward()
                 if (step + 1) % args.gradient_accumulation_steps == 0:
-                    lr_this_step = args.learning_rate * \
-                        warmup_linear(global_step/t_total,
-                                      args.warmup_proportion)
+                    lr_this_step = args.learning_rate * WarmupLinearSchedule(global_step / t_total,
+                                                                             args.warmup_proportion)
                     if args.fp16:
                         # modify learning rate with special warm up BERT uses
                         for param_group in optimizer.param_groups:
